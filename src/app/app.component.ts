f99d83924df8f8d32a325434ef3c528fafb38800
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-07-16 12:38:50 +0200

    no funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sportify';
-  isMenuOpen = true;
-  contentMargin = 240;
+  public title = 'sportify';
+  public isMenuOpen = true;
+  public contentMargin = 240;
+  public coach = false;
+  public player = true;
 
   task: string[] = [
     'Clearning out my closet', 'Take out trash bins', 'Wash car', 'Tank up the motorcycles', 'Go for flight training'
   ]
+
+  userChange() {
+    this.coach = true;
+    this.player = false;
+    console.log(this.coach)
+  }
 
   onToolbarMenuToggle() {
     console.log('On toolbar toggled', this.isMenuOpen);
     this.isMenuOpen = !this.isMenuOpen;
 
     if(!this.isMenuOpen) {
-      this.contentMargin = 70;
+      this.contentMargin = 0;
     } else {
       this.contentMargin = 240;
     }
