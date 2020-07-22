d244101d236dc4fb163fdcdf372cd91087b6f318
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-07-22 17:21:19 +0200

    form login

--- sportify-app/src/app/pages/login/login.component.ts
+++ sportify-app/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user'
+import { UserService } from '../../shared/user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user = new User;
+  public submitted = false;
 
-  constructor() { }
+  constructor(private apiService: UserService) { 
+    this.user
+  }
+
+  login(form) {
+    console.log(form.value);
+    this.apiService.postUser(this.user).subscribe((data) => {
+      console.log(data); 
+    })
+    this.submitted = true;
+  }
 
   ngOnInit(): void {
   }
