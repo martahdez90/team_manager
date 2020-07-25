0b8a093a8bfeff08646e708970e87b60c2ff7871
John <63423135+pablocanellas@users.noreply.github.com>
2020-07-25 13:45:35 +0200

    trainingPlayers

--- sportify-app/src/app/pages/my-workouts/my-workouts.component.ts
+++ sportify-app/src/app/pages/my-workouts/my-workouts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Training } from "../../models/training";
 import { LoginService } from 'src/app/shared/login.service';
+import { TrainingService } from 'src/app/shared/training-service.service';
 
 @Component({
   selector: 'app-my-workouts',
@@ -8,16 +9,63 @@ import { LoginService } from 'src/app/shared/login.service';
   styleUrls: ['./my-workouts.component.css']
 })
 export class MyWorkoutsComponent implements OnInit {
+
+ public training :Training;
+ dataBase:Object;
   public games = [new Training('Entreno de 2' ,new Date(2020, 7, 15), 'Madird', 'Comntario de prueba'),
                   new Training('Entreno de prueba 4' ,new Date(2020, 6, 1), 'Londres', 'Comntario para probar'),
                   new Training('Entreno de prueba 6' ,new Date(2020, 6, 1), 'Londres', 'Comntario para probar')]
                   
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private trainingService: TrainingService) { }
 
   newWorkout(){
     console.log(this.loginService.userLoged.user_id)
     this.games.push(new Training('Entreno de prueba' ,new Date(2050, 6, 1), 'Hawai', 'Comntario para probar'))
   }
+
+  getTrainings()
+  {
+    this.trainingService.getTrainings().subscribe((data)=>
+    {
+      this.dataBase=data;
+    });
+  };
+
+  getTraining(id:number)
+  {
+    this.trainingService.getTraining(id).subscribe((data)=>
+    {
+      this.dataBase=data;
+    });
+  };
+
+ addTraining(newTraining:Training)
+  {
+      this.trainingService.postTraining(newTraining).subscribe((data)=>
+      {
+        console.log(data);
+      });
+  };
+
+  UpdateTraining(newTraining:Training)
+  {
+    this.trainingService.putTraining(newTraining).subscribe((data)=>
+    {
+      console.log(data);
+    });
+  };
+
+  deleteTraining(id:number)
+  {
+    this.trainingService.deleteTraining(id).subscribe((data)=>
+    {
+      console.log(data);
+    });
+  };
+
+  
+
+
 
   ngOnInit(): void {
     
--- sportify-app/src/app/pages/trainings-player/trainings-player.component.ts
+++ sportify-app/src/app/pages/trainings-player/trainings-player.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/shared/login.service';
+import { TrainingService } from 'src/app/shared/training-service.service';
+import { Training } from 'src/app/models/training';
 
 @Component({
   selector: 'app-trainings-player',
@@ -7,8 +9,55 @@ import { LoginService } from 'src/app/shared/login.service';
   styleUrls: ['./trainings-player.component.css']
 })
 export class TrainingsPlayerComponent implements OnInit {
+  public training: Training;;
+ dataBase:Object;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,  private trainingService:TrainingService)
+   { }
+
+
+   getTrainings()
+   {
+     this.trainingService.getTrainings().subscribe((data)=>
+     {
+       this.dataBase=data;
+     });
+   };
+ 
+   getTraining(id:number)
+   {
+     this.trainingService.getTraining(id).subscribe((data)=>
+     {
+       this.dataBase=data;
+     });
+   };
+ 
+  addTraining(newTraining:Training)
+   {
+       this.trainingService.postTraining(newTraining).subscribe((data)=>
+       {
+         console.log(data);
+       });
+   };
+ 
+   UpdateTraining(newTraining:Training)
+   {
+     this.trainingService.putTraining(newTraining).subscribe((data)=>
+     {
+       console.log(data);
+     });
+   };
+ 
+   deleteTraining(id:number)
+   {
+     this.trainingService.deleteTraining(id).subscribe((data)=>
+     {
+       console.log(data);
+     });
+   };
+ 
+
+   
 
   ngOnInit(): void {
   }
