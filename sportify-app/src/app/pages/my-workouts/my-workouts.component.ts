b2ac5e2df8c76a633c71b23e0717c1cf4c697edd
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-07-29 18:57:05 +0200

    alerts borrar

--- sportify-app/src/app/pages/my-workouts/my-workouts.component.ts
+++ sportify-app/src/app/pages/my-workouts/my-workouts.component.ts
@@ -3,110 +3,116 @@ import { Training } from "../../models/training";
 import { LoginService } from 'src/app/shared/login.service';
 import { TrainingService } from 'src/app/shared/training-service.service';
 import { TeamService } from 'src/app/shared/team.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-my-workouts',
   templateUrl: './my-workouts.component.html',
   styleUrls: ['./my-workouts.component.css']
 })
+
 export class MyWorkoutsComponent implements OnInit {
 
   public edit: Training;
   public dataBase: object;
   public teams: object;
-  
-                  
+
+
   constructor(private loginService: LoginService, private trainingService: TrainingService, private teamService: TeamService) {
     this.edit = new Training("", "", "", "")
-   }
+  }
 
-  public getTraining(train: Training){
+  public getTraining(train: Training) {
     this.edit = train
     console.log(this.edit)
   }
 
-  public saveTraining(training_id:number)
-  {
+  public saveTraining(training_id: number) {
     console.log('entranmiento guardado')
     console.log(training_id)
-    this.loginService.training_id= training_id;
+    this.loginService.training_id = training_id;
   }
 
-  public addTraining(name:HTMLInputElement, date:HTMLInputElement, location:HTMLInputElement, description:HTMLInputElement, )
-  {
-    let newTraining=new Training(name.value,date.value,location.value,description.value);
-    newTraining.team_id= this.loginService.team_id;
+  public addTraining(name: HTMLInputElement, date: HTMLInputElement, location: HTMLInputElement, description: HTMLInputElement,) {
+    let newTraining = new Training(name.value, date.value, location.value, description.value);
+    newTraining.team_id = this.loginService.team_id;
     console.log(newTraining);
-      this.trainingService.postTraining(newTraining).subscribe((data)=>
-      {
-        console.log(data)
-        this.trainingService.getTeamTraining(this.loginService.team_id).subscribe((data)=>
-        {
-          console.log(data);
-          this.dataBase= data
-        })
-        
-      });
+    this.trainingService.postTraining(newTraining).subscribe((data) => {
+      console.log(data)
+      this.trainingService.getTeamTraining(this.loginService.team_id).subscribe((data) => {
+        console.log(data);
+        this.dataBase = data
+      })
+
+    });
   };
 
-  public putTraining(name:HTMLInputElement, date:HTMLInputElement, location:HTMLInputElement, description:HTMLInputElement)
-  {
+  public putTraining(name: HTMLInputElement, date: HTMLInputElement, location: HTMLInputElement, description: HTMLInputElement) {
     let newTraining = new Training(name.value, date.value, location.value, description.value)
     newTraining.training_id = this.edit.training_id
 
-    if( name.value === "" )
-    {
+    if (name.value === "") {
       newTraining.name = this.edit.name;
     }
-    if(date.value ==="" )
-    {
+    if (date.value === "") {
       newTraining.date = this.edit.date;
     }
-    if(location.value === "" )
-    {
+    if (location.value === "") {
       newTraining.location = this.edit.location;
     }
-    if(description.value === "" )
-    {
-      newTraining.description =this.edit.description;
+    if (description.value === "") {
+      newTraining.description = this.edit.description;
     }
-    this.trainingService.putTraining(newTraining).subscribe((data)=>
-    {
-      
+    this.trainingService.putTraining(newTraining).subscribe((data) => {
+
       console.log(data);
-      this.trainingService.getTeamTraining(this.loginService.userLoged.user_id).subscribe((data)=>
-      {
+      this.trainingService.getTeamTraining(this.loginService.userLoged.user_id).subscribe((data) => {
         console.log(data);
-        this.dataBase=data;
+        this.dataBase = data;
       });
     });
   };
 
-  public deleteTraining(id:number)
-  {
-    this.trainingService.deleteTraining(id).subscribe((data)=>
-    {
-      console.log(data)
-      this.trainingService.getTeamTraining(this.loginService.userLoged.user_id).subscribe((data)=>
-      {
-        console.log(data);
-        this.dataBase=data;
-      });
-    });
-  };
+  public deleteTraining(id: number) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás recuperarlo!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#00bfa5',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'borrar'
+    }).then((result) => {
+      if (result.value) {
+        console.log(id)
+        this.trainingService.deleteTraining(id).subscribe((data) => {
+          console.log(data)
+          this.trainingService.getTeamTraining(this.loginService.userLoged.user_id).subscribe((data) => {
+            console.log(data);
+            this.dataBase = data;
+            Swal.fire({
+              title: '¡Eliminado!',
+              text: 'Tu equipo ha sido borrado',
+              icon: 'success',
+              confirmButtonColor: '#00bfa5'
+            })
+          });
+        });
+      };
+    })
+  }
 
   ngOnInit(): void {
-    this.trainingService.getTeamTraining(this.loginService.team_id).subscribe((data)=>
-    {
+    this.trainingService.getTeamTraining(this.loginService.team_id).subscribe((data) => {
       console.log(data);
-      this.dataBase=data;
+      this.dataBase = data;
     });
 
     // this.teamService.getTeams(this.loginService.userLoged.user_id).subscribe((data)=>
     // {
     //   this.teams=data;
     // })
-    
+
   }
 
 }
