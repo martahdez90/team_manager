38302e9a73221d060ef7fc4a32bfb7513b9f1f06
Axelmanza <manzanares712@gmail.com>
2020-08-14 11:22:49 +0200

    botones de navegacion en las pestañas

--- sportify-app/src/app/app.component.ts
+++ sportify-app/src/app/app.component.ts
@@ -7,31 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'sportify';
-  public isMenuOpen = true;
-  public contentMargin = 240;
-  public coach = false;
-  public player = true;
-
-  userChange() {
-    this.coach = true;
-    this.player = false;
-    console.log(this.coach)
-  }
-
-  onToolbarMenuToggle() {
-    console.log('On toolbar toggled', this.isMenuOpen);
-    this.isMenuOpen = !this.isMenuOpen;
-
-    if(!this.isMenuOpen) {
-      this.contentMargin = 0;
-    } else {
-      this.contentMargin = 240;
-    }
-  }
-  // sidenavEvents(str) {
-  //   console.log(str);
-  // }
-
 
   constructor() { }
 
--- sportify-app/src/app/app.module.ts
+++ sportify-app/src/app/app.module.ts
@@ -7,23 +7,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { CoachMenuComponent } from './pages/coach-menu/coach-menu.component';
 import { PlayerMenuComponent } from './pages/player-menu/player-menu.component';
-import { MyCalendarComponent } from './pages/my-calendar/my-calendar.component';
 import { MyTeamsComponent } from './pages/my-teams/my-teams.component';
 import { MyPlayersComponent } from './pages/my-players/my-players.component';
 import { MyWorkoutsComponent } from './pages/my-workouts/my-workouts.component';
 import { MyExercisesComponent } from './pages/my-exercises/my-exercises.component';
 import { MyCoachComponent } from './pages/my-coach/my-coach.component';
 import { MySettingsComponent } from './pages/my-settings/my-settings.component';
-import { WhiteFooterComponent } from './pages/white-footer/white-footer.component';
 import { GreenFooterComponent } from './pages/green-footer/green-footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { ToolbarComponent } from './pages/toolbar/toolbar.component';
 import { MyGamesComponent } from './pages/my-games/my-games.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientModule } from "@angular/common/http";
@@ -37,44 +32,40 @@ import { PlayerSettingsComponent } from './pages/player-settings/player-settings
 
 //validadores npm i ng-validate-equal
 import { ValidateEqualModule } from 'ng-validate-equal';
+import { WhiteFooterComponent } from './pages/white-footer/white-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    WhiteFooterComponent,
     LandingPageComponent,
     LoginComponent,
     RegisterComponent,
     CoachMenuComponent,
     PlayerMenuComponent,
-    MyCalendarComponent,
     MyTeamsComponent,
     MyPlayersComponent,
     MyWorkoutsComponent,
     MyExercisesComponent,
     MyCoachComponent,
     MySettingsComponent,
-    WhiteFooterComponent,
     GreenFooterComponent,
-    ToolbarComponent,
     MyGamesComponent,
     TrainingsPlayerComponent,
     MatchesPlayerComponent,
     HomeComponent,
     LandingMenuComponent,
-    PlayerSettingsComponent,
-   
+    PlayerSettingsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule,
     MatListModule,
     MatCardModule,
     MatIconModule,
-    MatSidenavModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     HttpClientModule,
